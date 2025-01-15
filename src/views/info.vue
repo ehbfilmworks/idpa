a9c1c7f042f1de464bb3ac3553bea88e2f043923
Silas Hanyecz <silas.hanyecz@mysign.ch>
2025-01-15 11:26:38 +0100

    delete lorin und silas jpg

--- src/views/info.vue
+++ src/views/info.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="container" style="margin-top: 10%">
-    <h1 class="mb-5 ms-3">VideoAgenturName</h1>
+    <h2 class="mb-5 ms-3">Team</h2>
     <div class="container">
       <div class="row">
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
           <div class="our-team">
             <div class="picture">
-              <img src="/img/lorin.jpeg"  alt="lorin büchel"/>
+
             </div>
             <div class="team-content">
               <h3 class="name">Lorin Büchel</h3>
@@ -17,7 +17,7 @@
         <div class="col-12 col-sm-6 col-md-4 col-lg-3">
           <div class="our-team">
             <div class="picture">
-              <img class="img-fluid" src="/img/silas.jpg">
+
             </div>
             <div class="team-content">
               <h3 class="name">Silas Hanyecz</h3>
