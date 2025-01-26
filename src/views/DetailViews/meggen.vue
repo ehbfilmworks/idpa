f951f24802b94c1ac934b0c121938abc4da78444
Silas Hanyecz <silas.hanyecz@mysign.ch>
2025-01-26 19:16:26 +0100

    detail view of meggen and palace

---
+++ src/views/DetailViews/meggen.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container" style="margin-top: 10%">
+    <h2>Golfplatz Meggen</h2>
+    <div class="row">
+      <div class="col-md-10">
+        <p>Das Video für den Golfplatz in Meggen war unser erstes Projekt. Wir haben über die Mail-Adresse auf ihrer
+          Website
+          angefragt, ob wir im Rahmen unserer IDPA ein Image-Video für ihre Golfanlage drehen dürfen. Wir wurden an den
+          Inhaber weitergeleitet und haben sehr spontan mit ihm ein Datum für die Dreharbeiten abgemacht. </p>
+      </div>
+      <video src="/img/meggen_video_1080.mp4" controls class="w-50 mt-3 mb-3"></video>
+      <div class="col-md-10">
+        <p>Am Drehtag vor Ort wurden wir sehr herzlich empfangen. Der Inhaber hat uns alles erklärt und uns sogar eine
+          ganze
+          Ausrüstung inklusive eines Golf-Cart ausgeliehen. Auf dem Golfplatz mussten wir grosse Rücksicht auf die
+          Golf-Spieler nehmen, damit wir sie nicht stören. Wir erlebten viele tolle Begegnungen mit neugierigen Menschen
+          und
+          konnten ihnen dann erklären, aus welchem Grund wir da sind.</p>
+      </div>
+      <div class="col-md-4">
+        <p>Am Morgen war es in Meggen noch neblig, doch am Nachmittag kam die Sonne. Obwohl man es im Video erkennt, ist
+          es
+          nicht allzu schlimm, denn der grösste Teil des Videos ist es sonnig. </p>
+        <p>Nach ca. drei Wochen in denen wir das Video geschnitten, anderen Leuten gezeigt und dann verbessert haben,
+          schickten wir es zum Inhaber nach Meggen. Er war erstaunt und hatte grosse Freude. Jedoch konnten sie es noch
+          nicht einsetzen, da es jetzt im Winter nicht passt.</p>
+        <p>Model - Max Mariotto</p>
+      </div>
+      <div class="col-md-6">
+        <img src="/img/golfplatzmeggen.jpg" class="mt-3 mb-3 w-100">
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
---
+++ src/views/DetailViews/palace.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container" style="margin-top: 10%">
+    <h2>Hotel Palace Gstaad</h2>
+    <div class="row">
+      <div class="col-md-10">
+        <p>Dieses Projekt war ein Glückstreffer. An einem BM-Schultag im Dezember 2024 haben wir uns mit Sandra Hirschi,
+          einer Kollegin aus unserer BM-Klasse über unsere IDPA’s unterhalten. Nachdem wir ihr das Video vom Golfplatz
+          in
+          Meggen zeigten, war sie begeistert und meinte wir müssen unbedingt für ihren Arbeitgeber, das Hotel Palace in
+          Gstaad dasselbe tun. Auf diese Weise konnten wir dies einfädeln und fuhren am 15.12.2024 nach Gstaad, um
+          Videos
+          vom Hotel aufzunehmen. </p>
+      </div>
+      <div class="col-md-10">
+        <video src="/img/palace_video.mp4" controls class="w-50 mt-3 mb-3"></video>
+      </div>
+      <div class="col-md-10">
+        <p>Es war ein sehr aufregender, spannender und anstrengender Tag. Wir durften den SPA-Bereich, die Suiten, die
+          Zigarrenlounge, die Restaurants, den Coiffeur, das Fitness-Center und noch viel mehr filmen. Am Mittag assen
+          wir sogar mit der Tochter des Inhabers zu Mittag. Ihn selbst haben wir leider nicht persönlich
+          kennengelernt.
+          Jan Jansen vom Marketing-Team des Hotels war den ganzen Tag mit uns unterwegs, unterstützte uns und stellte
+          sich sogar noch als Model zur Verfügung. </p>
+        <p>Das Video war nach vier Wochen fertig und sie hatten grosse Freude daran. Möglicherweise dürfen wir in
+          Zukunft ein Video in einem anderen Format für sie realisieren. </p>
+      </div>
+      <div class="col-md-10">
+        <div class="d-flex justify-content-center">
+          <img src="/img/HotelPalace.jpg" class="mt-3 w-50 ">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script setup>
+</script>
