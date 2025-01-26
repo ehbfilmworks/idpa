21365904ac73af269f02b0f9a4615a172e1cfd56
Silas Hanyecz <silas.hanyecz@mysign.ch>
2025-01-26 20:52:22 +0100

    sticky footer

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -2,7 +2,7 @@
 import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
 </script>
 
-<template class="footer-section">
+<template class="footer-section" style="position: fixed">
   <div class="container">
     <div class="footer-cta pt-5 pb-5">
       <div class="row">
@@ -21,8 +21,8 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
             <div class="cta-text">
               <h4>Follow Us</h4>
               <div class="footer-social-icon">
-                <a href="#" class="m-0 p-0"><font-awesome-icon :icon="['far', 'envelope']" style="color: white" /></a>
-                <a href="#" class="m-0 p-0 ps-3"><font-awesome-icon :icon="['fab', 'instagram']" style="color: white"/></a>
+                <a href="mailto:ehbfilmworks@gmail.com" class="m-0 p-0"><font-awesome-icon :icon="['far', 'envelope']" style="color: white" /></a>
+                <a href="https://www.instagram.com/ehb_filmworks?igsh=MXV2ZDU1aXJrdnJhaA==" class="m-0 p-0 ps-3"><font-awesome-icon :icon="['fab', 'instagram']" style="color: white"/></a>
               </div>
             </div>
           </div>
@@ -32,7 +32,7 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
             <i class="far fa-envelope-open"></i>
             <div class="cta-text">
               <h4>Mail us</h4>
-              <a  class="m-0 p-0 pt-5" href="mailto:ehbfilmworks@gmail.com" style="color: white; font-size: 15px">ehb@gmail.com</a>
+              <a  class="m-0 p-0 pt-5" href="mailto:ehbfilmworks@gmail.com" style="color: white; font-size: 15px">ehbfilmworks@gmail.com</a>
             </div>
           </div>
         </div>
@@ -44,15 +44,6 @@ import {FontAwesomeIcon} from "@fortawesome/vue-fontawesome";
       <div class="row">
         <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
           <div class="footer-menu">
-            <ul>
-              <li>
-                <router-link to="/">Home</router-link>
-              </li>
-              <li><a href="#">Terms</a></li>
-              <li><a href="#">Privacy</a></li>
-              <li><a href="#">Policy</a></li>
-              <li><a href="#">Contact</a></li>
-            </ul>
           </div>
         </div>
       </div>
