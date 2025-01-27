eb184f4fd36e2efc87db8e69a93251b980c10203
brave187 <166106241+brave187@users.noreply.github.com>
2025-01-27 21:44:52 +0100

    Update contact.vue

--- src/views/contact.vue
+++ src/views/contact.vue
@@ -19,7 +19,7 @@
       <div class="col-sm d-flex flex-column align-items-end">
         <div class="pe-4 ps-4">
           <p style="opacity: 1">EHB - Film works</p>
-          <p>ehb@gmail.com</p>
+          <p>ehbfilmworks@gmail.ch</p>
           <p>+41 78 187 33 33</p>
         </div>
         <iframe class="w-100" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3215.2778646583865!2d7.537453376370382!3d47.205339571155875!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4791d7fc1c849875%3A0x883a29fdb0709207!2sGewerblich-industrielle%20Berufsschule%20Solothurn!5e1!3m2!1sde!2sch!4v1731245568202!5m2!1sde!2sch" height="200" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
